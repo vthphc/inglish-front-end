9837ceeda385a401f7de313322e4aa17d54e8cd4
tkj3nfuq <thinphcv5@gmail.com>
2024-09-03 21:09:11 +0700

    init project

---
+++ src/components/SampleComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SampleComponent() {
+  return (
+    <div>SampleComponent</div>
+  )
+}
---
+++ src/pages/SamplePage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SamplePage() {
+  return (
+    <div className='text-4xl'>SamplePage</div>
+  )
+}
