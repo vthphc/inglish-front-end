0656892f214394879899b0c241c85c5553edbb1e
Mingu <vhnm3004@outlook.com>
2024-09-26 19:21:14 +0700

    migrated to vite

--- src/pages/SamplePage.jsx
+++ src/pages/SamplePage.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SamplePage() {
   return (
-    <div className='text-4xl'>SamplePage</div>
+    <div className='text-4xl text-red-500'>SamplePage</div>
   )
 }
