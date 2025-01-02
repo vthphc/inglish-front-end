505abbf0746bc6d3f7a97c636600a7bd80415253
tkj3nfuq <thinphcv5@gmail.com>
2025-01-02 15:41:52 +0700

    Add Node.js engine specification and refactor Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		screens: {
-			"xs": "475px",
-			// => @media (min-width: 475px) { ... }
+import withMT from "@material-tailwind/react/utils/withMT";
+import daisyui from "daisyui";
 
-			"sm": "640px",
-			// => @media (min-width: 640px) { ... }
-
-			"md": "768px",
-			// => @media (min-width: 768px) { ... }
-
-			"lg": "1024px",
-			// => @media (min-width: 1024px) { ... }
-
-			"xl": "1280px",
-			// => @media (min-width: 1280px) { ... }
-
-			"2xl": "1536px",
-			// => @media (min-width: 1536px) { ... }
-		},
-		extend: {
-			fontFamily: {
-				'roboto': ["Roboto Mono", "serif"]
-			}
-		},
-	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		themes: ["light"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		base: true, // applies background color and foreground color for root element by default
-		styled: true, // include daisyUI colors and design decisions for all components
-		utils: true, // adds responsive and modifier utility classes
-		prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-		themeRoot: ":root", // The element that receives theme color CSS variables
-	},
+export default withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "475px", // => @media (min-width: 475px) { ... }
+      sm: "640px", // => @media (min-width: 640px) { ... }
+      md: "768px", // => @media (min-width: 768px) { ... }
+      lg: "1024px", // => @media (min-width: 1024px) { ... }
+      xl: "1280px", // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px", // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto Mono", "serif"],
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"], // false: only light + dark | true: all themes | array: specific themes
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames
+    logs: true, // shows info about daisyUI version and config in the console
+    themeRoot: ":root", // the element that receives theme color CSS variables
+  },
 });
